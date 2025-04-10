597b1cff7a9cde3709b2079811ab4a526b300bc4
Doviaks <dovydas.nar@gmail.com>
2025-04-10 20:55:44 +0300

    filter and search

--- sport_results/basketball_results/views.py
+++ sport_results/basketball_results/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from .models import BasketballMatch
+from django.db.models import Q
 
 
 def match_list(request):
     matches = BasketballMatch.objects.all().order_by('-date')
-    return render(request, 'match_list.html', {'matches': matches})
+
+    query_team = request.GET.get('team')
+    query_date = request.GET.get('date')
+
+    if query_team:
+        matches = matches.filter(
+            Q(team1__icontains=query_team) | Q(team2__icontains=query_team))
+
+    if query_date:
+        matches = matches.filter(date=query_date)
+
+    return render(request, 'match_list.html', {'matches': matches, 'query_team': query_team or '', 'query_date': query_date or ''})
+
+
+
+
+
+
 
 
 
