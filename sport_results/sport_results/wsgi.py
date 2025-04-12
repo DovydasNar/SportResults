6f1e89c4463223ddd2f6637a134ebfb0a6b3aa63
Doviaks <dovydas.nar@gmail.com>
2025-04-12 15:14:56 +0300

    update for test deploy

--- sport_results/sport_results/wsgi.py
+++ sport_results/sport_results/wsgi.py
@@ -14,7 +14,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sport_results.settings')
+
 
 application = get_wsgi_application()
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sport_results.sport_results.settings')
