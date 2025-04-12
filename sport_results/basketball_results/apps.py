903a650e6492e92acf46baac8317384615478c68
Doviaks <dovydas.nar@gmail.com>
2025-04-12 14:53:51 +0300

    update for test deploy

--- sport_results/basketball_results/apps.py
+++ sport_results/basketball_results/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class BasketballResultsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'basketball_results'
+    name = 'sport_results.basketball_results'
+
